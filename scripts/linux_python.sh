6228e53ab8fd78e2c4cf2f79f7e73f610f8bfd20
pllittle <shadowsaver911@gmail.com>
2022-04-08 14:00:12 -0400

    update

---
+++ scripts/linux_python.sh
@@ -0,0 +1,188 @@
+#!/bin/sh
+
+[ ! -z $src_python ] && [ $src_python -eq 1 ] \
+	&& return 0
+
+for fn in install; do
+	. $HOME/github/baSHic/scripts/$fn.sh
+done
+
+# Python Functions
+install_python(){
+	local version v1 pkg pkg_ver apps_dir cmd
+	local url inst_dir down_dir load_env
+	
+	install_args $@ -p Python -d "2.7.6, 3.8.1, 3.8.4" || return 0
+	url=https://www.python.org/ftp/python/${version}/Python-${version}.tgz
+	v1=$(echo $version | cut -d '.' -f1-2)
+	
+	if [ $load_env -eq 1 ]; then
+		[ ! -f $inst_dir/lib/pkgconfig/python-$v1.pc ] \
+			&& return 1
+		[ ! -f $inst_dir/bin/python${v1} ] \
+			&& echo -e "Install $pkg_ver" >&2 \
+			&& return 1
+		update_env -e PKG_CONFIG_PATH -a "$inst_dir/lib/pkgconfig"
+		local pc_fn
+		for pc_fn in `ls $inst_dir/lib/pkgconfig | grep "python-$v1" | sed 's|.pc$||g'`; do
+			pkg-config --exists --print-errors $pc_fn >&2 \
+				|| return 1
+			CPPFLAGS="$CPPFLAGS `pkg-config --cflags $pc_fn`"
+			LDFLAGS="$LDFLAGS `pkg-config --libs $pc_fn`"
+		done
+		update_env -e PATH -a "$inst_dir/bin"
+		update_env -e LD_LIBRARY_PATH -a "$inst_dir/lib"
+		return 0
+	fi
+	
+	extract_url -u $url -a $apps_dir -s $pkg_ver
+	[ $? -eq 1 ] && return 0
+	[ -d $down_dir ] && rm -rf $down_dir
+	mv $inst_dir ~/downloads/
+	cd $inst_dir
+	
+	# Set environment
+	clear_env
+	local PYTHONPATH PYTHONHOME
+	local CPPFLAGS LDFLAGS
+	install_gcc -a $apps_dir -e \
+		&& install_ncurses -a $apps_dir -e \
+		&& install_readline -a $apps_dir -e \
+		&& install_xz -a $apps_dir -e \
+		&& install_bzip2 -a $apps_dir -e \
+		|| return 1
+	
+	# Install
+	cmd="$down_dir/configure"
+	[ ! -z "$CPPFLAGS" ] && cmd="$cmd CPPFLAGS=\"$CPPFLAGS\""
+	[ ! -z "$LDFLAGS" ] && cmd="$cmd LDFLAGS=\"$LDFLAGS\""
+	cmd="$cmd --prefix=$inst_dir >&2"
+	cmd="$cmd && make >&2 && make install >&2"
+	eval $cmd
+	
+	local status=$?
+	install_wrapup -s $status -i $inst_dir -d $down_dir
+	return $status
+	
+}
+install_pip(){
+	echo "Debug code" >&2 && return 1
+	local version apps_dir py_dir
+	local PYTHONPATH PYTHONHOME
+	local url=https://bootstrap.pypa.io/pip/get-pip.py
+	local inst_fn=get-pip.py
+	
+	while [ ! -z $1 ]; do
+		case $1 in
+			-v | --version )
+				shift
+				version="$1"
+				;;
+			-a | --apps_dir )
+				shift
+				apps_dir="$1"
+				;;
+		esac
+		shift
+	done
+	
+	if [ -z $version ]; then
+		make_menu -p "Which python version? (e.g. 2.7.6, 3.8.4)"
+		read version
+	fi
+	[ -z $apps_dir ] && apps_dir=$HOME/apps
+	
+	py_dir=$apps_dir/Python-${version}
+	if [ ! -d $py_dir ]; then
+		install_python -v ${version} -a $apps_dir
+	fi
+	
+	# Add python/bin to PATH
+	update_env -e PATH -a "$py_dir/bin"
+	
+	# local PYTHONPATH=$py_dir/lib/python${v2}:$py_dir/lib/python${v2}/site-packages:$PYTHONPATH
+	
+	# Check pip already installed
+	if [ -f $py_dir/bin/pip ]; then
+		pip install -U pip >&2
+	else
+		new_mkdir ~/downloads
+		cd ~/downloads
+		[ ! -f $inst_fn ] && wget --no-check-certificate $url >&2
+		python $inst_fn >&2
+		new_rm $HOME/downloads/$inst_fn
+	fi
+	
+}
+install_pymod(){
+	echo "Debug code" >&2 && return 1
+	
+	local version apps_dir v2 py_dir pylib mod mods
+	local cnt=0
+	
+	while [ ! -z $1 ]; do
+		case $1 in
+			-v | --version )
+				shift
+				version="$1"
+				;;
+			-a | --apps_dir )
+				shift
+				apps_dir="$1"
+				;;
+			-m | --modules )
+				while [ ! -z "$2" ]; do
+					case $2 in
+						-* )
+							break
+							;;
+						* )
+							mods[cnt]="$2"
+							let cnt=cnt+1
+							shift
+							;;
+					esac
+				done
+				;;
+		esac
+		shift
+	done
+	
+	if [ -z $version ]; then
+		make_menu -p "Which python version? (e.g. 2.7.6, 3.8.4)"
+		read version
+	fi
+	[ -z $apps_dir ] && apps_dir=$HOME/apps
+	
+	py_dir=$apps_dir/Python-${version}
+	if [ ! -d $py_dir ]; then
+		install_python -v ${version} -a $apps_dir
+	fi
+	v2=`echo $version | cut -d '.' -f1-2`
+	pylib=$py_dir/lib/python${v2}
+	
+	# Check/update pip and add python to PATH
+	install_pip -v $version -a $apps_dir
+	
+	# Add PYTHONPATH??
+	# update_env -e PYTHONPATH -a "$pylib" "$pylib/site-packages"
+	
+	# Install modules
+	for mod in "${mods[@]}"; do
+		pip install $mod >&2
+	done
+	
+	# Remove python from PATH
+	update_env -e PATH -r "$py_dir/bin"
+	
+	return 0
+	
+}
+
+
+
+
+src_python=1
+
+###
+
---
+++ scripts/progress.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+smart_progress(){
+	local cnt tot iter job_status string iter2
+	local mult
+	
+	while [ ! -z "$1" ]; do
+		case $1 in
+			-c | --cnt )
+				shift
+				cnt="$1"
+				;;
+			-t | --tot )
+				shift
+				tot="$1"
+				;;
+			-j | --iter )
+				shift
+				iter="$1"
+				;;
+			-m | --mult )
+				shift
+				mult="$1"
+				;;
+			-r | --string )
+				shift
+				string="$1"
+				;;
+			-s | --job_status )
+				shift
+				job_status="$1"
+				;;
+		esac
+		shift
+	done
+	
+	[ -z $cnt ] 				&& echo "Add -c <iteration cnt>" >&2 && return 1
+	[ -z $iter ] 				&& echo "Add -j <iter to print>" >&2 && return 1
+	[ -z $tot ] 				&& echo "Add -t <total iterations>" >&2 && return 1
+	[ -z $job_status ] 	&& echo "Add -s <job_status>" >&2 && return 1
+	[ -z $mult ] 				&& mult=1
+	iter2=$((iter * mult))
+	
+	if [ -z $string ]; then
+		string="."
+	else
+		string="$string "
+	fi
+	
+	if [ "$job_status" == "incomplete" ]; then
+		echo -ne "${white}$string${NC}" >&2
+	elif [ "$job_status" == "notdone" ]; then
+		echo -ne "${red}$string${NC}" >&2
+	elif check_array $job_status running pending; then
+		echo -ne "${cyan}$string${NC}" >&2
+	elif [ "$job_status" == "complete" ]; then
+		echo -ne "$string" >&2
+	else
+		echo -ne "${yellow}$string${NC}" >&2
+	fi
+	
+	[ $((cnt % iter)) -eq 0 -o $cnt -eq $tot ] \
+		&& echo -ne "$cnt out of $tot " >&2
+	[ $((cnt % iter2)) -eq 0 -o $cnt -eq $tot ] \
+		&& echo -ne "\n" >&2
+	
+}
+
+###
+
