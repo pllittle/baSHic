9c6f0ffb568bfc211cc68fb6bb337e43f9bf6a40
pllittle <shadowsaver911@gmail.com>
2022-04-11 22:50:06 -0400

    bedtool

--- scripts/genomic.sh
+++ scripts/genomic.sh
@@ -59,7 +59,89 @@ install_htslib(){
 	return $status
 	
 }
-
+install_samtools(){
+	local version v1 pkg pkg_ver apps_dir status cmd
+	local url inst_dir down_dir load_env
+	
+	install_args $@ -p samtools -d 1.15.1; status=$?
+	[ $status -eq 2 ] && return 0; [ ! $status -eq 0 ] && return 1
+	url=https://github.com/samtools/samtools/releases/download
+	url=$url/$version/samtools-$version.tar.bz2
+	
+	# Load environment
+	if [ $load_env -eq 1 ]; then
+		[ ! -f $inst_dir/bin/samtools ] \
+			&& echo -e "Install $pkg_ver" >&2 \
+			&& return 1
+		update_env -e PATH -a "$inst_dir/bin"
+		return 0
+	fi
+	
+	extract_url -u $url -a $apps_dir -s $pkg_ver
+	[ $? -eq 1 ] && return 0
+	mv $down_dir $inst_dir
+	cd $inst_dir
+	
+	# Set environment
+	clear_env
+	local CPPFLAGS LDFLAGS
+	cmd=$(prep_env_cmd -a $apps_dir -p gcc libtool \
+		ncurses xz)
+	eval $cmd >&2 || return 1
+	
+	# Install
+	cmd="./configure"
+	[ ! -z "$CPPFLAGS" ] && cmd="$cmd CPPFLAGS=\"$CPPFLAGS\""
+	[ ! -z "$LDFLAGS" ] && cmd="$cmd LDFLAGS=\"$LDFLAGS\""
+	cmd="$cmd --prefix=$inst_dir >&2"
+	cmd="$cmd && make >&2 && make install >&2"
+	eval $cmd
+	
+	status=$?
+	install_wrapup -s $status -i $inst_dir -d $down_dir
+	return $status
+	
+}
+install_bedtools(){
+	local version v1 pkg pkg_ver apps_dir status cmd
+	local url inst_dir down_dir load_env tmp_dir
+	
+	install_args $@ -p bedtools -d 2.30.0; status=$?
+	[ $status -eq 2 ] && return 0; [ ! $status -eq 0 ] && return 1
+	url=https://github.com/arq5x/bedtools2/releases/download
+	url=$url/v$version/bedtools-$version.tar.gz
+	
+	# Load environment
+	if [ $load_env -eq 1 ]; then
+		[ ! -f $inst_dir/bin/bedtools ] \
+			&& echo -e "Install $pkg_ver" >&2 \
+			&& return 1
+		update_env -e PATH -a "$inst_dir/bin"
+		return 0
+	fi
+	
+	extract_url -u $url -a $apps_dir -s $pkg_ver
+	[ $? -eq 1 ] && return 0
+	tmp_dir=$(ls $apps_dir/downloads | grep $pkg)
+	tmp_dir=$apps_dir/downloads/$tmp_dir
+	mv $tmp_dir $down_dir; mv $down_dir $inst_dir
+	cd $inst_dir
+	
+	# Set environment
+	clear_env
+	local CPPFLAGS LDFLAGS
+	cmd=$(prep_env_cmd -a $apps_dir -p gcc libtool xz)
+	eval $cmd >&2 || return 1
+	
+	# Install
+	make -e VERBOSE=true CPPFLAGS="$CPPFLAGS" \
+		LDFLAGS="$LDFLAGS" >&2
+	
+	status=$?
+	install_wrapup -s $status -i $inst_dir -d $down_dir
+	return $status
+	
+}
 
 src_genomic=1
 
