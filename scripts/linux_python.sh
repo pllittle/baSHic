ae999809e7c08b2bbfcb3e04b012a1c9e082551a
pllittle <shadowsaver911@gmail.com>
2022-04-14 21:50:40 -0400

    ssl

--- scripts/linux_python.sh
+++ scripts/linux_python.sh
@@ -12,7 +12,7 @@ install_openssl(){
 	local version v1 pkg pkg_ver apps_dir status
 	local url inst_dir down_dir load_env cmd
 	
-	install_args $@ -p openssl -d 3.0.2; status=$?
+	install_args $@ -p openssl -d "1.0.2, 3.0.2"; status=$?
 	[ $status -eq 2 ] && return 0; [ ! $status -eq 0 ] && return 1
 	v1=$(echo $version | cut -d '.' -f1-2)
 	url=https://www.openssl.org/source/openssl-$version.tar.gz
@@ -52,13 +52,10 @@ install_openssl(){
 	
 	# Install
 	cmd="$down_dir/config --prefix=$inst_dir"
-	# cmd="$cmd --openssldir=$inst_dir"
+	cmd="$cmd --openssldir=$inst_dir"
 	[ ! -z "$CPPFLAGS" ] && cmd="$cmd CPPFLAGS=\"$CPPFLAGS\""
 	[ ! -z "$LDFLAGS" ] && cmd="$cmd LDFLAGS=\"$LDFLAGS\""
 	cmd="$cmd >&2 && make >&2 && make test >&2"
-	eval $cmd
-	echo -e "status = $?" >&2
-	echo "Continue install code" >&2 && return 0
 	cmd="$cmd && make install >&2"
 	eval $cmd
 	
