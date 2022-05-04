907b68de99abc28f702162f0d5c1d305e13c701e
pllittle <shadowsaver911@gmail.com>
2022-05-04 13:54:34 -0400

    add strelka2 soma

--- scripts/genomic.sh
+++ scripts/genomic.sh
@@ -9,7 +9,7 @@ for fn in install; do
 	. $git_dir/baSHic/scripts/$fn.sh
 done
 
-# Genomics functions
+# Installation functions
 install_htslib(){
 	local version v1 pkg pkg_ver apps_dir status cmd
 	local url inst_dir down_dir load_env
@@ -211,6 +211,130 @@ install_strelka2(){
 	fi
 	
 }
+
+# Execution functions
+run_strelka2_soma(){
+	local gatk_dir strelka_dir nbam tbam ref out_dir ncores
+	local regions confirm config_fn var_dir status
+	
+	confirm=0
+	while [ ! -z $1 ]; do
+		case $1 in
+			-s | --strelka_dir )
+				shift
+				strelka_dir="$1"
+				;;
+			-g | --gatk_dir )
+				shift
+				gatk_dir="$1"
+				;;
+			-t | --tbam )
+				shift
+				tbam="$1"
+				;;
+			-n | --nbam )
+				shift
+				nbam="$1"
+				;;
+			-r | --ref )
+				shift
+				ref="$1"
+				;;
+			-o | --out_dir )
+				shift
+				out_dir="$1"
+				;;
+			-c | --ncores )
+				shift
+				ncores="$1"
+				;;
+			--regions )
+				shift
+				regions="$1"
+				;;
+			--confirm )
+				confirm=1
+				;;
+			--config_fn )
+				shift
+				config_fn="$1"
+				;;
+		esac
+		shift
+	done
+	
+	# Check inputs
+	[ -z $strelka_dir ] && echo "Add -s <Strelka2 dir>" >&2 && return 1
+	[ -z $gatk_dir ] 		&& echo "Add -g <GATK dir>" >&2 && return 1
+	[ -z $tbam ] 				&& echo "Add -t <tumor bam>" >&2 && return 1
+	[ -z $nbam ] 				&& echo "Add -n <normal bam>" >&2 && return 1
+	[ -z $ref ] 				&& echo "Add -r <reference genome>" >&2 && return 1
+	[ -z $out_dir ] 		&& echo "Add -o <output dir>" >&2 && return 1
+	[ -z $ncores ] 			&& echo "Add -c <number of threads/cores>" >&2 && return 1
+	var_dir=$out_dir/results/variants
+	[ -s $out_dir/somatic.vcf.gz ] && return 0
+	
+	# Prepare regions
+	[ ! -z "$regions" ] \
+		&& regions=$(echo $regions | sed 's|^|,|g' | sed 's|,| --region |g' | sed 's|^ ||g')
+	
+	# Clear out any older stuff
+	new_rm $out_dir/results $out_dir/workspace \
+		$out_dir/runWorkflow* $out_dir/workflow*
+	
+	# Configure strelka stuff
+	local cmd
+	cmd="$strelka_dir/bin/configureStrelkaSomaticWorkflow.py"
+	cmd="$cmd --normalBam $nbam --tumorBam $tbam --referenceFasta $ref"
+	cmd="$cmd --disableEVS --exome"
+	[ ! -z $config_fn ] && cmd="$cmd --config $config_fn"
+	[ ! -z "$regions" ] && cmd="$cmd $regions"
+	cmd="$cmd --runDir $out_dir >&2"
+	
+	if [ $confirm -eq 1 ]; then
+		local resp
+		echo -e "`date`: Strelka Command:\n\n$cmd\n\n" >&2
+		make_menu -y -p "Does this look good?"; read resp
+		[ -z $resp ] && return 1
+		[ ! -z $resp ] && [ ! $resp -eq 1 ] && return 1
+	fi
+	
+	# Run variant calling
+	if [ ! -f $var_dir/somatic.snvs.vcf.gz ] || [ ! -f $var_dir/somatic.indels.vcf.gz ]; then
+		eval $cmd
+		status=$?
+		[ ! $status -eq 0 ] && echo -e "`date`: Strelka configure error" >&2 && return 1
+	
+		export OMP_NUM_THREADS=$ncores
+		$out_dir/runWorkflow.py -m local -j $ncores >&2
+		status=$?
+		[ $status -eq 0 ] \
+			&& echo -e "`date`: Strelka2 completed" >&2 \
+			|| echo -e "`date`: Strelka2 incomplete" >&2
+		[ ! $status -eq 0 ] && return 1
+	fi
+	
+	# Merge SNV and INDEL vcfs
+	export OMP_NUM_THREADS=1
+	$gatk_dir/gatk MergeVcfs \
+		--INPUT $var_dir/somatic.snvs.vcf.gz \
+		--INPUT $var_dir/somatic.indels.vcf.gz \
+		--OUTPUT $out_dir/somatic.vcf.gz >&2
+	status=$?
+	[ ! $status -eq 0 ] && echo -e "`date`: Error with MergeVcfs" >&2 && return 1
+	if [ ! -s $out_dir/somatic.vcf.gz ]; then
+		new_rm $out_dir/somatic.vcf.gz
+		echo -e "`date`: Empty vcf" >&2 && return 1
+	fi
+	
+	# Clean up Strelka stuff
+	new_rm $out_dir/results $out_dir/workspace \
+		$out_dir/runWorkflow* $out_dir/workflow*
+	
+	return 0
+	
+}
+
 
 src_genomic=1
 
